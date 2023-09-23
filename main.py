88c33bf4d34e73b0cb701d2d3cb63bd81d045f57
oyakovenko <alex231@ukr.net>
2023-09-23 13:36:22 +0300

    Create minimal PDF for each Exel file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("orders/*.xlsx")
 print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1", engine='openpyxl')
-    print(df)
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    f_name = Path(filepath).stem
+    order_num = f_name.split('-')[0]
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Order number {order_num}')
+    pdf.output(f"PDFs/{f_name}.pdf")
