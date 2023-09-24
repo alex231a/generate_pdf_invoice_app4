63a70b73394ecb5877dbb070ae35961f5a20a122
oyakovenko <alex231@ukr.net>
2023-09-24 18:03:59 +0300

    Final version additional task

--- create_PDF_from_many_txt_files/create_PDF_from_many_txt.py
+++ create_PDF_from_many_txt_files/create_PDF_from_many_txt.py
@@ -6,9 +6,15 @@ filepaths = glob.glob("./*.txt")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for filepath in filepaths:
+    with open(filepath, 'r') as file:
+        content = file.read()
     pdf.add_page()
     f_name = Path(filepath).stem
     title = f_name.title()
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f'{title}')
+    pdf.cell(w=50, h=8, txt=f'{title}', ln=1)
+
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 pdf.output(f"output.pdf")
