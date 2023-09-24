022df602e1106c7deb655a0378126ddcb40fff7a
oyakovenko <alex231@ukr.net>
2023-09-24 16:47:09 +0300

    add invoice date in pdf

--- main.py
+++ main.py
@@ -8,10 +8,19 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1", engine='openpyxl')
+
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+
     f_name = Path(filepath).stem
-    order_num = f_name.split('-')[0]
+    f_name_list = f_name.split('-')
+    order_num = f_name_list[0]
+    f_date = f_name_list[1]
+
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f'Order number {order_num}')
+    pdf.cell(w=50, h=8, txt=f'Order number {order_num}', ln=1)
+
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Date {f_date}', ln=1)
+
     pdf.output(f"PDFs/{f_name}.pdf")
